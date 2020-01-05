1c55094d107a305096eb6ddf15767532414805cf
MooradAltamimi <moorad2003@gmail.com>
2020-01-05 16:42:25 +0000

    First commit. Initalised React files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<NavBar/>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+	render() {
+		return (<nav className='nav'></nav>);
+	}
+}
+
+export default NavBar;
